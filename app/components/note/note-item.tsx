29fa093aa1c0d6262f1022d008848daabad13491
CiroMirkin <ciromirkin@gmail.com>
2026-08-25 16:19:46 -0300

    fix: note item UI problem

--- app/components/note/note-item.tsx
+++ app/components/note/note-item.tsx
@@ -20,7 +20,7 @@ export default function NoteItem({ note, subjectName }: Props) {
             }}
             asChild
         >
-            <View className="flex-1 flex-col gap-2 rounded-lg border-2 bg-[#abdef1] p-2 px-3">
+            <View className="flex-col gap-2 rounded-lg border-2 bg-[#abdef1] p-2 px-3">
                 <Text numberOfLines={1} className="text-base text-black">
                     {firstLine(note.content)}
                 </Text>
